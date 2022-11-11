8714915408469d1ad8b7c81086a97e2ed077479c
spiritofenrgy <spiritofenergy220@gmail.com>
2022-11-11 10:02:55 +0300

    CardDaysTemp

--- app/src/main/java/com/kodex/weatherappcompose/MainActivity.kt
+++ app/src/main/java/com/kodex/weatherappcompose/MainActivity.kt
@@ -1,28 +1,157 @@
 package com.kodex.weatherappcompose
 
+/*import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kodex.weatherappcompose.screens.MainScreen
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.kodex.weatherappcompose.data.WeatherModel
+import com.kodex.weatherappcompose.screens.MainCard
+import com.kodex.weatherappcompose.screens.TabLayout
 import com.kodex.weatherappcompose.ui.theme.WeatherAppComposeTheme
+import org.json.JSONObject*/
 
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.kodex.weatherappcompose.data.WeatherModel
+import com.kodex.weatherappcompose.screens.MainCard
+import com.kodex.weatherappcompose.screens.TabLayout
+import com.kodex.weatherappcompose.ui.theme.WeatherAppComposeTheme
+import org.json.JSONObject
+
+
+const val API_KEY = "4db8308b3cd94aa685270311222706"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppComposeTheme {
-                MainScreen()
+                val daysList = remember {
+                    mutableStateOf(listOf<WeatherModel>())
+                }
+                val currentDay = remember {
+                    mutableStateOf(WeatherModel(
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "")
+                    )
+                }
+                getData("Moscow",this, daysList, currentDay)
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.weather_bg
+                    ),
+                    contentDescription = "im1",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.5f),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column{
+                    MainCard(currentDay)
+                   TabLayout(daysList)
+                }
+
+            }
         }
     }
 }
+
+private fun getData(city: String, context: Context,
+                    daysList: MutableState<List<WeatherModel>>, currentDay: MutableState<WeatherModel>){
+    val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY" +
+            "&q=$city" +
+            "&days=" +
+            "3" +
+            "&aqi=no&alerts=no"
+    val queue = Volley.newRequestQueue(context)
+    val sRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        {
+            response ->
+           val list = getWeatherByDays(response)
+            currentDay.value = list[0]
+            daysList.value = list
+        },
+        {
+            Log.d("checkData", "Error: $it")
+        }
+    )
+    queue.add(sRequest)
+}
+private fun getWeatherByDays(response: String): List<WeatherModel>{
+        if(response.isEmpty()) return listOf()
+        val list = ArrayList<WeatherModel>()
+        val mainObject = JSONObject(response)
+        val city = mainObject.getJSONObject(
+            "location").getString(
+            "name")
+        val days = mainObject.getJSONObject(
+            "forecast").getJSONArray(
+            "forecastday")
+
+    for (i in 0 until days.length()) {
+        val item = days[i] as JSONObject
+        list.add(
+            WeatherModel(
+                city,
+                item.getString("date"),
+                "",
+                item.getJSONObject("day")
+                    .getJSONObject("condition")
+                    .getString("text"),
+                item.getJSONObject("day")
+                    .getJSONObject("condition")
+                    .getString("icon"),
+                item.getJSONObject("day")
+                    .getString("maxtemp_c"),
+                item.getJSONObject("day")
+                    .getString("mintemp_c"),
+                item.getJSONArray("hour")
+                    .toString()
+            )
+        )
     }
-@Preview(showBackground = true)
-@Composable
-fun MainScreen() {}
+        list[0] = list[0].copy(
+        time = mainObject.getJSONObject("current").getString("last_updated"),
+        currentTemp = mainObject.getJSONObject("current").getString("temp_c")
+        )
+        return list
+    }
+
+
+
+
--- app/src/main/java/com/kodex/weatherappcompose/screens/MainScreen.kt
+++ app/src/main/java/com/kodex/weatherappcompose/screens/MainScreen.kt
@@ -1,41 +1,40 @@
 package com.kodex.weatherappcompose.screens
 
-import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
 import com.kodex.weatherappcompose.R
+import com.kodex.weatherappcompose.data.WeatherModel
 import com.kodex.weatherappcompose.ui.theme.BlueLite
+import kotlinx.coroutines.launch
 
-@Preview(showBackground = true)
 @Composable
-fun MainScreen() {
-     Image(painter = painterResource(
-         id = R.drawable.weather_bg),
-         contentDescription = null,
-     modifier = Modifier
-         .fillMaxSize()
-         .alpha(0.5f),
-            contentScale = ContentScale.FillBounds)
+fun MainCard(currentDay: MutableState<WeatherModel>) {
+
     Column (
         modifier = Modifier
-            .fillMaxSize()
             .padding(4.dp)
-            ){
+    ) {
         Card(
             modifier = Modifier.fillMaxWidth(),
             backgroundColor = BlueLite,
@@ -46,25 +45,134 @@ fun MainScreen() {
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
                         modifier = Modifier.padding(8.dp),
-                        text = "20 Jun 2022 13:00",
-                    style = TextStyle(fontSize = 16.sp),
-                    color = Color.White
+                        text = currentDay.value.time,
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
                     )
                     AsyncImage(
-                        model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                        model = "https:" + currentDay.value.icon,
                         contentDescription = "im2",
-                        modifier = Modifier.size(35.dp).padding(end = 8.dp, start = 8.dp))
-
+                        modifier = Modifier.size(50.dp)
+                            .padding(
+                                end = 8.dp,
+                                start = 8.dp
+                            )
+                    )
                 }
+                Text(
+                    text = currentDay.value.city,
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = currentDay.value.currentTemp,
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = currentDay.value.condition ,
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        onClick = {
 
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "im3",
+                            tint = Color.White
+                        )
+                    }
+                    Text(
+                        text = "${currentDay.value.maxTemp}ºC / ${currentDay.value.minTemp}ºC",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_sync),
+                            contentDescription = "im4",
+                            tint = Color.White
+                        )
+                    }
+                }
             }
-
         }
+    }
+}
 
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabLayout(daysList: MutableState<List<WeatherModel>>){
+    val tabList = listOf("HOURS", "DAYS")
+    val pagerState = rememberPagerState()
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .padding(
+                start = 5.dp,
+                end = 5.dp
+            )
+            .clip(RoundedCornerShape(5.dp))
+    ) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = { pos ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, pos)
+                )
+            },
+            backgroundColor = BlueLite,
+            contentColor = Color.White
+        ) {
+            tabList.forEachIndexed{index, text ->
+                Tab(
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = {
+                        Text(text = text)
+                    }
+                )
+            }
+        }
+        HorizontalPager(
+            count = tabList.size,
+            state = pagerState,
+            modifier = Modifier.weight(1.0f)
+        ) { index ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                itemsIndexed(
+                    daysList.value
+                ){ _, item ->
+                    ListItem(item)
+                }
+            }
+        }
     }
 }
